3c366402a366bcd51922c12132ce524d84119dbb
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2021-09-30 09:57:53 +0530

    :bug:

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -20,7 +20,7 @@ const publicVapidKey =
 const urlB64ToUint8Array = base64String => {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
   const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/')
-  const rawData = atob(base64)
+  const rawData = Buffer.from(base64)
   const outputArray = new Uint8Array(rawData.length)
   for (let i = 0; i < rawData.length; ++i) {
     outputArray[i] = rawData.charCodeAt(i)
